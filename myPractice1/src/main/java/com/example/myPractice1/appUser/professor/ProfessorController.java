ae0e6e0b4feedcdd32403f4cde40aa5a09b4575f
Florin <f.schikarski@tu-bs.de>
2022-11-01 00:07:09 +0100

    removed batch imports

--- myPractice1/src/main/java/com/example/myPractice1/appUser/professor/ProfessorController.java
+++ myPractice1/src/main/java/com/example/myPractice1/appUser/professor/ProfessorController.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
+// import org.springframework.batch.core.BatchStatus;
+// import org.springframework.batch.core.Job;
+// import org.springframework.batch.core.JobExecution;
+// import org.springframework.batch.core.JobParameter;
+// import org.springframework.batch.core.JobParameters;
+// import org.springframework.batch.core.JobParametersInvalidException;
+// import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+// import org.springframework.batch.core.launch.JobLauncher;
+// import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+// import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+// import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +29,7 @@ import lombok.Setter;
 
 @RestController
 @RequestMapping("/test/appUser/professor")
-@EnableBatchProcessing
+// @EnableBatchProcessing
 @Getter
 @Setter
 public class ProfessorController {
