e8a823189b5018fc168af6300639c4f82a55d899
Florin <f.schikarski@tu-bs.de>
2022-11-02 19:25:58 +0100

    clean up and delete everything

--- myPractice1/src/main/java/com/example/myPractice1/appUser/Employee.java
+++
@@ -1,6 +0,0 @@
-package com.example.myPractice1.appUser;
-
-
-public interface Employee {
-    
-}
--- myPractice1/src/test/java/com/example/myPractice1/MyPractice1ApplicationTests.java
+++ myPractice1/src/test/java/com/example/myPractice1/MyPractice1ApplicationTests.java
@@ -1,41 +1,13 @@
 package com.example.myPractice1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.example.myPractice1.appUser.professor.Professor;
-import com.example.myPractice1.appUser.professor.ProfessorService;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class MyPractice1ApplicationTests {
-	@Autowired
-	private MockMvc mockMvc;
-	@Autowired
-	private ProfessorService professorService;
 
-
-	// @Test
-	void contextLoads() throws Exception {
-		mockMvc.perform(get("/test/appUser/professor/getByUserName?userName=florin.schikarski@gmail.com"))
-			   .andDo(print())
-			   .andExpect(status().isOk());
-	}
-
-
-	// @Test
-	void testService() throws Exception {
-		Professor professor = (Professor) professorService.loadUserByUsername("florin.schikarski@gmail.com");
-		int salary = 520;
-		assertEquals(salary, professor.getSalary());
-	}
+	
 }
